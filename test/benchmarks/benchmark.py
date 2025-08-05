a16b63cb3591da74e780e4e6a312e4b6d41bfdcf
Daniel Duncan <danieldunc@hotmail.com>
2025-08-05 14:52:56 +1000

    Outlined benchmark.py

---
+++ test/benchmarks/benchmark.py
@@ -0,0 +1,11 @@
+# TODO: generic benchmarking function that can be called and configured for specific benchmarks
+
+# TODO: take scheme keygen, sign, verify functions, and setup parameters
+
+# TODO: call and time keygen
+
+# TODO: call and time sign
+
+# TODO: call and time verify
+
+# TODO: return (t keygen, t sign, t verify, t total)
