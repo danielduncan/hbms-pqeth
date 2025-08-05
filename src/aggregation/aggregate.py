a6033e5516efea07cf20213c5d562835ae09dfda
Daniel Duncan <danieldunc@hotmail.com>
2025-08-05 21:06:01 +1000

    Clarified aggregate_signatures

--- src/aggregation/aggregate.py
+++ src/aggregation/aggregate.py
@@ -1,5 +1,6 @@
 # TODO: aggregate signatures
-def aggregate_signatures(public_keys, signatures):
+def aggregate_signatures(signatures):
+    # signatures[i] = (public_key of ith signer, associated signature)
     # TODO: aggregate the signatures
     aggregated_signature = "placeholder"
 
