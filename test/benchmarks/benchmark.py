6b10320740ed80f82f85188b9a1122000601b900
Daniel Duncan <danieldunc@hotmail.com>
2025-08-05 21:11:22 +1000

    Benchmark stub

--- test/benchmarks/benchmark.py
+++ test/benchmarks/benchmark.py
@@ -1,11 +1,21 @@
+import time
+
 # TODO: generic benchmarking function that can be called and configured for specific benchmarks
+def benchmark(scheme_keygen, scheme_sign, scheme_verify, setup_params):
+    t_start = time.time()
 
-# TODO: take scheme keygen, sign, verify functions, and setup parameters
+    t_keygen_start = time.time()
+    scheme_keygen(**setup_params)
+    t_keygen = time.time() - t_keygen_start
 
-# TODO: call and time keygen
+    t_sign_start = time.time()
+    scheme_sign(**setup_params)
+    t_sign = time.time() - t_sign_start
 
-# TODO: call and time sign
+    t_verify_start = time.time()
+    scheme_verify(**setup_params)
+    t_verify = time.time() - t_verify_start
 
-# TODO: call and time verify
+    t_total = time.time() - t_start
 
-# TODO: return (t keygen, t sign, t verify, t total)
+    return (t_keygen, t_sign, t_verify, t_total)
