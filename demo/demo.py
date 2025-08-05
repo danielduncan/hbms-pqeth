0aa1eb474e747c90e3482f8e5a69d44918e35dfa
Daniel Duncan <danieldunc@hotmail.com>
2025-08-05 19:15:51 +1000

    Draft demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,3 +1,6 @@
+from individual import generate_key, sign_message
+from aggregation import aggregate_signatures, verify_signature
+
 # TODO: demo script
 # TODO: print results at each step
 def main():
@@ -5,14 +8,19 @@ def main():
     message = "placeholder"
 
     # TODO: generate some validator keys
+    keys = [generate_key() for _ in range(10)]
 
     # TODO: sign message with keys
+    signatures = [(pk, sign_message(sk, message)) for (pk, sk) in keys]
 
     # TODO: aggregate signatures
+    aggregated_signature = aggregate_signatures(signatures)
 
     # TODO: verify aggregated signature
+    valid = verify_signature(aggregated_signature, message)
 
     # TODO: print final result, time, and signature size (compare with an without aggregation)
+    print(f"{'valid' if valid else 'invalid'} signature")
 
 if __name__ == "__main__":
     main()
