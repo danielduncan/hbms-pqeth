28069930ca53ad151df6365c7ac258d19f767ee5
Daniel Duncan <danieldunc@hotmail.com>
2025-08-05 14:56:51 +1000

    Outlined demo.py

---
+++ demo/demo.py
@@ -0,0 +1,11 @@
+# TODO: demo script
+
+# TODO: generate some validator keys
+
+# TODO: sign shared message with keys
+
+# TODO: aggregate signatures
+
+# TODO: verify aggregated signature
+
+# TODO: print results at each step
