c80221b5adaac64410b547e9531f7a2235534ef8
Daniel Duncan <danieldunc@hotmail.com>
2025-08-05 19:04:17 +1000

    Started demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,12 +1,18 @@
 # TODO: demo script
-
-# TODO: generate some validator keys
-
-# TODO: sign shared message with keys
-
-# TODO: aggregate signatures
-
-# TODO: verify aggregated signature
-
 # TODO: print results at each step
-# TODO: print final result, time, and signature size (compare with an without aggregation)
+def main():
+    # take some input message to validate
+    message = "placeholder"
+
+    # TODO: generate some validator keys
+
+    # TODO: sign message with keys
+
+    # TODO: aggregate signatures
+
+    # TODO: verify aggregated signature
+
+    # TODO: print final result, time, and signature size (compare with an without aggregation)
+
+if __name__ == "__main__":
+    main()
