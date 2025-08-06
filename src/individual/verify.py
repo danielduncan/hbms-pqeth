94fdc2cf7942f2d7c364b76c250aa75256399767
Daniel Duncan <danieldunc@hotmail.com>
2025-08-06 14:32:21 +1000

    Working simple WOTS

--- src/individual/verify.py
+++ src/individual/verify.py
@@ -1,4 +1,12 @@
-# TODO: verify signature
-def verify_signature(signature, message):
-    # TODO: implement signature verification
-    return False
+# verify signature - simple Winternitz One-Time Signature
+from src.individual.utils import H
+
+def verify_signature(sig, message, pk):
+    h = H(message.encode('ascii'))
+    x = int.from_bytes(h, 'big')
+
+    j = (2 ** 16 - 1) - x
+
+    validity = (pk == H(sig, j))
+
+    return validity
