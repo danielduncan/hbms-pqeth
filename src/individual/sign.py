6320b61a03f81331535999ced1ea7d16c8959a04
Daniel Duncan <danieldunc@hotmail.com>
2025-08-06 14:31:28 +1000

    Working simple WOTS

--- src/individual/sign.py
+++ src/individual/sign.py
@@ -1,4 +1,11 @@
-# TODO: message signing
-def sign_message(secret_key, message):
-    # TODO: sign the message with sk
-    return message
+# TODO: message signing - simple Winternitz One-Time Signature
+from src.individual.utils import H
+
+# w = 16 # message split into chunks of w bits
+
+def sign_message(sk, message):
+    h = H(message.encode('ascii'))
+    x = int.from_bytes(h, 'big')
+    sig = H(sk, x)
+
+    return sig
