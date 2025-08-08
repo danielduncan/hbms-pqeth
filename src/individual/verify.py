f7d2168a988736d9f00508e6060600d8bf57bbd4
Harrison Liu <Demagonz@hotmail.com>
2025-08-08 11:08:29 +1000

    restoring verify_signature

--- src/individual/verify.py
+++ src/individual/verify.py
@@ -20,25 +20,7 @@ def verify_wots(sig: List[bytes], message: str) -> bytes:
     return H(b''.join(pks), tweak=HashTweaks.CHAIN.value)
 
 def verify_signature(sig: List[bytes], message: str, pk: bytes) -> bool:
-    w: int = (len(message) // l) * 8 # chunk width in bits
-    T: int = 0 # T is a target value used for integrity rather than checksum
-    target: int = l * (2 ** w - 1) / 2
-
-    chunk_pks: List[bytes] = []
-    for i, chunk in enumerate(textwrap.wrap(message, w)):
-        h = H(chunk.encode('ascii'))
-        x = int.from_bytes(h, 'big')
-        T += x
-
-        j = (2 ** 16 - 1) - x
-        chunk_pk = H(sig[i], j)
-        chunk_pks.append(chunk_pk)
-
-    print(f"T={T} vs {target}")
-    if T != target:
-        return False # signature has been modified
-
-    return pk == H(b''.join(chunk_pks))
+    return pk == verify_wots(sig, message)
 
 # eXtended Merkle Signature Scheme (XMSS) signature verification
 def xmss_verify(sig: Tuple[int, List[bytes], List[bytes]], message: str, pk: bytes) -> bool:
