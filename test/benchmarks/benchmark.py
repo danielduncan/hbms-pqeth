6aa00fb532ad75caeebdfc4f492f09d5a34a226c
Callie Morgan <z5361443@ad.unsw.edu.au>
2025-08-11 19:04:29 +1000

    updated benchmark script

--- test/benchmarks/benchmark.py
+++ test/benchmarks/benchmark.py
@@ -6,10 +6,8 @@ import string
 import random
 from hashlib import sha256
 
-n = 50 # number of iterations
-w = 128
+n = 10 # number of iterations
 l = 2
-k = 5 # tree depth
 message_length = 32
 
 # TODO: generic benchmarking function that can be called and configured for specific benchmarks
@@ -32,7 +30,8 @@ def benchmark(scheme_keygen, scheme_sign, scheme_verify, setup_params):
 
     return (t_keygen, t_sign, t_verify, t_total)
 
-def benchmark():
+def benchmark(k, w):
+    t0 = 0
     t1 = 0
     t2 = 0
     for _ in range(n):
@@ -42,7 +41,12 @@ def benchmark():
         message = ''.join(random.choices(string.ascii_letters + string.digits, k = message_length)).encode('ascii')
         SCHEME = HBMS(H=sha256, N=1, n=256, w=w, k=k)
         s = Scheme(SCHEME)
+
+        # xmss_keygen
+        start = time.perf_counter()
         sks, pk, paths = s.keygen(epoch)
+        end = time.perf_counter()
+        t0 += end - start
 
         # xmss_sign
         start = time.perf_counter()
@@ -57,12 +61,15 @@ def benchmark():
         valid = s.verify(sig, message, pk)
         end = time.perf_counter()
         t2 += end - start
+    t0 /= n
     t1 /= n
     t2 /= n
-    return [t1 * 1000, t2 * 1000]
+    return [t0 * 1000, t1 * 1000, t2 * 1000]
 
 with open('test/benchmarks/xmss_res.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['algorithm name',' sign time (ms)', ' verify time (ms)'])
-    results = ['xmss'] + benchmark()
-    writer.writerow(results)
+    writer.writerow(['algorithm name', ' chunk length', ' tree depth', ' keygen time (ms)', ' sign time (ms)', ' verify time (ms)'])
+    for w in [8, 16, 32, 64]:
+        for k in [6, 12, 18]:
+            results = ['xmss', w, k] + benchmark(k, w)
+            writer.writerow(results)
